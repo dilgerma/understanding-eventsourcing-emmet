46ad548258a59fdce52bbf63074f03fd8aaa0e11
Martin Dilger <martin@nebulit.de>
2025-07-23 22:32:43 +0200

    fix duplicate keys

---
+++ src/pages/screen.tsx
@@ -0,0 +1,12 @@
+import ScreenComponent from '../screens/screen/ScreenComponent';
+import {Navigation} from "../components/navigation/Navigation";
+import {commonGetServerSideProps} from "../supabase/ProtectedPageProps";
+import {GetServerSidePropsContext} from "next";
+
+
+export default function Screen(props: any) {
+        return <ScreenComponent/>
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext):Promise<any> => {
+    return commonGetServerSideProps(context)}
---
+++ src/pages/title.tsx
@@ -0,0 +1,12 @@
+import TitleComponent from '../screens/title/TitleComponent';
+import {Navigation} from "../components/navigation/Navigation";
+import {commonGetServerSideProps} from "../supabase/ProtectedPageProps";
+import {GetServerSidePropsContext} from "next";
+
+
+export default function Title(props: any) {
+        return <TitleComponent/>
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext):Promise<any> => {
+    return commonGetServerSideProps(context)}
